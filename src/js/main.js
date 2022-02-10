ead1fd15979dddbc9c9152a5bfd3c6518e4338bb
Timur Svoboda <svobodatimur@gmail.com>
2022-02-11 00:42:44 +0200

    Update

--- src/js/main.js
+++ src/js/main.js
@@ -11,6 +11,48 @@ document.addEventListener("DOMContentLoaded", () => {
   document.addEventListener("click", e => {
     if (e.target.classList.contains("switch")) {
       e.target.classList.toggle("switch_turn_on");
+      
+      const ariaChecked = e.target.getAttribute("aria-checked");
+      if (ariaChecked && ariaChecked === "false") {
+        e.target.setAttribute("aria-checked", "true");
+      } else {
+        e.target.setAttribute("aria-checked", "false");
+      }
     }
   });
+
+  // burger
+  document.addEventListener("click", e => {
+    const burger = e.target.closest(".burger");
+
+    if (burger) {
+      burger.classList.toggle("burger_cross");
+
+      const ariaPressed = burger.getAttribute("aria-pressed")
+      if (ariaPressed && ariaPressed === "false") {
+        burger.setAttribute("aria-pressed", "true");
+      } else {
+        burger.setAttribute("aria-pressed", "false");
+      }
+    }
+  });
+
+  // connect #mobile-menu-burger to #mobile-menu
+  const mobileMenuBurger = document.querySelector("#mobile-menu-burger");
+  const mobileMenu = document.querySelector("#mobile-menu");
+  if (mobileMenuBurger && mobileMenu) {
+    mobileMenuBurger.addEventListener("click", () => mobileMenu.classList.toggle("mobile-menu_open"));
+  }
+
+  // mobile-menu__scroll-area_shadow
+  document.addEventListener("scroll", e => {
+    const mobileMenuScrollArea = e.target;
+    if (mobileMenuScrollArea.classList.contains("mobile-menu__scroll-area")) {
+      if (mobileMenuScrollArea.scrollTop !== 0) {
+        mobileMenuScrollArea.classList.add("mobile-menu__scroll-area_shadow")
+      } else {
+        mobileMenuScrollArea.classList.remove("mobile-menu__scroll-area_shadow")
+      }
+    }
+  }, true)
 });
