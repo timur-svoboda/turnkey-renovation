7ae15d63cbe9289db854a7f568cbf2a625420d4c
Timur Svoboda <svobodatimur@gmail.com>
2022-02-14 21:03:08 +0200

    Update

--- src/js/main.js
+++ src/js/main.js
@@ -67,7 +67,10 @@ document.addEventListener("DOMContentLoaded", () => {
       document.querySelectorAll(".burger").forEach(item => item.classList.toggle("burger_theme_dark"));
       document.querySelectorAll(".mobile-menu").forEach(item => item.classList.toggle("mobile-menu_theme_dark"));
       document.querySelectorAll(".first-screen").forEach(item => item.classList.toggle("first-screen_theme_dark"));
-      document.querySelectorAll(".button").forEach(item => item.classList.toggle("button_theme_dark"));
+      document.querySelectorAll(".button").forEach(button => {
+        button.classList.toggle("button_theme_light");
+        button.classList.toggle("button_theme_dark");
+      });
       document.querySelectorAll(".title").forEach(item => item.classList.toggle("title_theme_dark"));
       document.querySelectorAll(".arrow-link").forEach(item => item.classList.toggle("arrow-link_theme_dark"));
       document.querySelectorAll(".section-about").forEach(item => item.classList.toggle("section-about_theme_dark"));
@@ -83,6 +86,30 @@ document.addEventListener("DOMContentLoaded", () => {
       document.querySelectorAll(".workflow-step").forEach(item => {
         item.classList.toggle("workflow-step_theme_light");
         item.classList.toggle("workflow-step_theme_dark");
+      });
+      document.querySelectorAll(".tab-bar").forEach(tabBar => {
+        tabBar.classList.toggle("tab-bar_theme_light");
+        tabBar.classList.toggle("tab-bar_theme_dark");
+      });
+      document.querySelectorAll(".section-calculator").forEach(tabBar => {
+        tabBar.classList.toggle("section-calculator_theme_light");
+        tabBar.classList.toggle("section-calculator_theme_dark");
+      });
+      document.querySelectorAll(".calculator").forEach(tabBar => {
+        tabBar.classList.toggle("calculator_theme_light");
+        tabBar.classList.toggle("calculator_theme_dark");
+      });
+      document.querySelectorAll(".range").forEach(tabBar => {
+        tabBar.classList.toggle("range_theme_light");
+        tabBar.classList.toggle("range_theme_dark");
+      });
+      document.querySelectorAll(".radio").forEach(tabBar => {
+        tabBar.classList.toggle("radio_theme_light");
+        tabBar.classList.toggle("radio_theme_dark");
+      });
+      document.querySelectorAll(".checkbox-list").forEach(tabBar => {
+        tabBar.classList.toggle("checkbox-list_theme_light");
+        tabBar.classList.toggle("checkbox-list_theme_dark");
       });
     });
   }
@@ -148,4 +175,213 @@ document.addEventListener("DOMContentLoaded", () => {
       }
     });
   });
+
+  // tab-bar
+  document.addEventListener("mouseenter", e => {
+    if (e.target.classList && e.target.classList.contains("tab-bar__tab")) {
+      e.target.classList.add("tab-bar__tab_hover");
+    }
+  }, true);
+
+  document.addEventListener("mouseleave", e => {
+    if (e.target.classList && e.target.classList.contains("tab-bar__tab")) {
+      e.target.classList.remove("tab-bar__tab_hover");
+    }
+  }, true);
+
+  document.addEventListener("click", e => {
+    const tab = e.target;
+    if (e.target.classList && e.target.classList.contains("tab-bar__tab")) {
+      if (!tab.classList.contains("tab-bar__tab_active")) {
+        const tabBar = tab.closest(".tab-bar");
+        Array.from(tabBar.querySelectorAll(".tab-bar__tab")).forEach(tab => {
+          tab.classList.remove("tab-bar__tab_active");
+          tab.setAttribute("aria-selected", "false");
+        });
+        tab.classList.add("tab-bar__tab_active");
+        tab.setAttribute("aria-selected", "true");
+        tabBar.dataset.value = tab.dataset.value;
+        tabBar.dispatchEvent(new CustomEvent("change", {
+          bubbles: true,
+          value: () => tabBar.dataset.value
+        }));
+      }
+    }
+  });
+
+  // button 
+  document.addEventListener("mouseenter", e => {
+    if (e.target.classList && e.target.classList.contains("button")) {
+      if (e.target.classList.contains("button_theme_light")) {
+        e.target.classList.add("button_theme_light-hover");
+      } else if (e.target.classList.contains("button_theme_dark")) {
+        e.target.classList.add("button_theme_dark-hover");
+      }
+    }
+  }, true);
+
+  document.addEventListener("mouseleave", e => {
+    if (e.target.classList && e.target.classList.contains("button")) {
+      if (e.target.classList.contains("button_theme_light")) {
+        e.target.classList.remove("button_theme_light-hover");
+      } else if (e.target.classList.contains("button_theme_dark")) {
+        e.target.classList.remove("button_theme_dark-hover");
+      }
+    }
+  }, true);
+
+  // section-calculator
+  const calculatorSlider = document.querySelector(".section-calculator__slider");
+  if (calculatorSlider) {
+    const slider = new Swiper(calculatorSlider, {
+      allowTouchMove: false
+    });
+
+    const calculatorTabBar = document.querySelector(".section-calculator__tab-bar");
+    if (calculatorTabBar) {
+      calculatorTabBar.addEventListener("change", () => {
+        const value = calculatorTabBar.dataset.value;
+        switch (value) {
+          case "standard":
+            slider.slideTo(0);
+            break;
+          case "comfort":
+            slider.slideTo(1);
+            break;
+          case "premium":
+            slider.slideTo(2);
+            break;
+        }
+      });
+    }
+  }
+
+  document.addEventListener("change", e => {
+    if (e.target.classList && e.target.classList.contains("calculator__range")) {
+      const output = e.target.nextElementSibling;
+      output.value = e.target.dataset.value + " м2"
+    }
+  });
+
+  {
+    const planPrice = document.querySelector(".section-calculator__plan-price");
+    const tabBar = document.querySelector(".section-calculator__tab-bar");
+    const range = document.querySelector(".section-calculator__range");
+    const radio = document.querySelector(".section-calculator__radio");
+    const checkboxList = document.querySelector(".section-calculator__checkbox-list");
+    if (planPrice && tabBar && range && radio && checkboxList) {
+      function calcPrice() {
+        let base = 4000;
+
+        switch (tabBar.dataset.value) {
+          case "standard":
+            base *= 1;
+            break;
+          case "comfort":
+            base *= 1.2;
+            break;
+          case "premium":
+            base *= 1.5;
+            break;
+        }
+
+        base *= 1 + (range.dataset.value - range.dataset.min) / (range.dataset.max - range.dataset.min);
+
+        base *= 1 + +("0." + (radio.dataset.value - 1));
+
+        const checkboxValues = checkboxList.dataset.values === "" ? [] : checkboxList.dataset.values.split(",");
+        base *= 1 + +("0." + (checkboxValues.length));
+
+        return Math.round(base);
+      }
+
+      document.addEventListener("change", e => {
+        if (e.target === tabBar || e.target === range || e.target === radio || e.target === checkboxList) {
+          planPrice.value = calcPrice();
+        }
+      });
+    }
+  }
+  
+
+  // range
+  document.addEventListener("mousedown", e => {
+    if (e.target.classList && e.target.classList.contains("range__area")) {
+      const range = e.target.closest(".range");
+      const circle = range.querySelector(".range__circle");
+      
+      circle.style.left = e.offsetX + "px";
+      range.dataset.value = Math.round(+range.dataset.min + (+range.dataset.max - +range.dataset.min) * e.offsetX / e.target.offsetWidth);
+      
+      range.dispatchEvent(new CustomEvent("change", {
+        bubbles: true,
+        value: () => +range.dataset.value
+      }));
+    }
+  });
+  
+  document.addEventListener("mousedown", e => {
+    if (e.target.classList && e.target.classList.contains("range__circle-area")) {
+      e.target.closest(".range__circle").dataset.drag = true;
+    }
+  });
+
+  document.addEventListener("mouseup", () => {
+    const circle = document.querySelector(".range__circle[data-drag=true]");
+    if (circle) {
+      circle.dataset.drag = false
+    }
+  });
+
+  document.addEventListener("mousemove", e => {
+    const circle = document.querySelector(".range__circle[data-drag=true]");
+    if (circle) {
+      const range = circle.closest(".range");
+
+      const left = Math.round(Math.max(0, Math.min(e.clientX - range.getBoundingClientRect().left, range.offsetWidth)));
+      circle.style.left = left + "px";
+      
+      range.dataset.value = Math.round(+range.dataset.min + (+range.dataset.max - +range.dataset.min) * left / range.offsetWidth);
+      range.dispatchEvent(new CustomEvent("change", {
+        bubbles: true,
+        value: () => +range.dataset.value
+      }));
+    }
+  });
+
+  // radio
+  document.addEventListener("click", e => {
+    if (e.target.classList && e.target.classList.contains("radio__item")) {
+      const radio = e.target.closest(".radio");
+      radio.querySelector(".radio__item_active").classList.remove("radio__item_active");
+      e.target.classList.add("radio__item_active");
+      radio.dataset.value = e.target.dataset.value;
+      radio.dispatchEvent(new CustomEvent("change", {
+        bubbles: true,
+        value: () => radio.dataset.value
+      }));
+    }
+  })
+
+  // checkbox-list
+  document.addEventListener("click", e => {
+    if (e.target.classList && e.target.classList.contains("checkbox-list__checkbox")) {
+      const checkboxList = e.target.closest(".checkbox-list");
+      const values = !checkboxList.dataset.values ? [] : checkboxList.dataset.values.split(",");
+
+      e.target.classList.toggle("checkbox-list__checkbox_active");
+
+      if (!values.includes(e.target.dataset.value)) {
+        values.push(e.target.dataset.value);
+      } else {
+        values.splice(values.indexOf(e.target.dataset.value), 1);
+      }
+
+      checkboxList.dataset.values = values.join(",");
+      checkboxList.dispatchEvent(new CustomEvent("change", {
+        bubbles: true,
+        value: () => values
+      }));
+    }
+  })
 });
